053a48d9f1380c8a8c37166821c206ad4e167e8f
david <davidszx318@gmail.com>
2019-08-06 22:32:06 -0700

    commit for changes

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,4 +5,12 @@ public class Controller {
     //this should be the 2nd commit
 
     // this is 3rd commit after first merge
+
+    //this is 4th commit
+
+    //this is 5th commit
+
+    //something happen for stash
+
+    //test if gitignore works asdfsd
 }
