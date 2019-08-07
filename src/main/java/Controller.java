9718c28e0a079d34bf1e8961366ee40e78912280
david <davidszx318@gmail.com>
2019-08-06 21:38:52 -0700

    3rd commit after merge
    5th commit
    
    6th commit
    q
    Q
    :Q

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,4 +3,11 @@ public class Controller {
     //this should be the 1st commit
 
     //this should be the 2nd commit
+
+    // this is 3rd commit after first merge
+
+    //this is 4th commit
+
+    //this is 5th commit
+
 }
